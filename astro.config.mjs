5d0de05eb5ae778514d10b6d69ab0bcfe42cd8cd
Usuario6842 <136459003+Usuario6842@users.noreply.github.com>
2024-09-24 10:15:26 -0400

    chore: configure deno adapter for SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
+import deno from '@deno/astro-adapter';
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  output: 'server',
+  adapter: deno()
 });
